701aac2b5f1581c5cc1291ca72adedb95f328652
João Porto <joaofporto@tecnico.ulisboa.pt>
2019-03-13 21:58:10 +0000

    Added tests

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std; // Bora tipo nao escrever 'std::' em tudo ya?
 
-int main() 
+// ###################### Grafos ################################################
+typedef unsigned int Int;
+
+class Router;
+
+class Router
 {
-    cout << "IPM";
+    int _id;
+    vector<Router *> routers;
+
+  public:
+    Router(Int id) : _id(id) {}
+};
+
+int main()
+{
+    Int routers_num = 0;
+    Int connect_num = 0;
+
+    printf("Reading\n");
+
+    scanf("%d", &routers_num);
+    scanf("%d", &connect_num);
+    printf("Routers: %d\nConnections: %d\n", routers_num, connect_num);
+
+    while (connect_num-- > 0)
+    {
+        Int rout1 = 0, rout2 = 0;
+        scanf("%d %d", rout1, rout2);
+    }
     return 0;
 }
