23e8c0276b6d0daf195a39dfdcccdef32d82a5f0
tigasgon1999 <31102413+tigasgon1999@users.noreply.github.com>
2019-03-20 22:33:50 +0000

    Finished commenting.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <stack>
 
-//#define EXISTS(a, n) (find(a.rbegin(), a.rend(), n) != a.end())
 #define MIN(a, b) (a < b ? a : b)
 
 using namespace std;
@@ -16,26 +15,26 @@ using namespace std;
 * Size of biggest SCC if all artic. points decide to collectively fuck off to a better place
 */
 
-// ###################### Global variables ################################################
+// ###################### Global variables ####################################################
 class Node;
-int nodesNum;
-int connectNum;
-vector<Node *> nodes;
-vector<int *> SCCs; // {max ID of graph, number of nodes}
-int sccCount;
-int apCount;
-int biggestSCC;
-stack<int> stackL;
-int *d;
-int *low;
-int *parent;
-bool *ap;
-bool *stackU;
+int nodesNum;           // Number of vertices
+int connectNum;         // Number of edges
+vector<Node *> nodes;   // Vector of vertices
+vector<int *> SCCs;     // Vector of SCCs, each containing {max ID of graph, number of nodes}
+int sccCount;           // Number of SCCs
+int apCount;            // Number of articulation points
+int biggestSCC;         // Size of the biggest SCC
+stack<int> stackL;      // Stack for the Tarjan Algorithm
+int *d;                 // Array d for the Tarjan Algorithm
+int *low;               // Array low for the Tarjan Algorithm
+int *parent;            // Array that contains the parent of each vertex
+bool *ap;               // Checklist  a points is an articulation point
+bool *stackU;           // Checkilst if a vertex is in the stack
 
-// ###################### Node ################################################
+// ###################### Node ################################################################
 class Node
 {
-    vector<int> _connects;
+    vector<int> _connects;  // Vector of adjacencies of the vertex
 
   public:
     Node() { _connects = vector<int>(); }
@@ -44,9 +43,10 @@ class Node
     ~Node() { _connects.clear(); }
 };
 
-// ###################### Input Handling ################################################
+// ###################### Input Handling ######################################################
 void readInput()
 {
+    // Read the number of vertexes and edges
     if (!scanf("%u", &nodesNum) || !scanf("%u", &connectNum))
         exit(-1);
     nodes.resize(nodesNum);
@@ -55,59 +55,54 @@ void readInput()
     for (int i = 0; i < nodesNum; i++)
         nodes[i] = new Node();
 
+
+    // Update the adjancies list of each vertex
     int routes[2] = {0, 0};
     while (scanf("%u %u", &routes[0], &routes[1]) > 0)
     {
         --connectNum;
-        // Update routes
         (*nodes[routes[0] - 1]).addConnection(routes[1] - 1);
         (*nodes[routes[1] - 1]).addConnection(routes[0] - 1);
     }
-    /*// Initialize the nodes
-    for (int i = 0; i < nodesNum; i++)
-        (*nodes[i]).getConnections().shrink_to_fit();
-    */
 
     if (connectNum != 0)
         exit(-1);
 }
 
-// ###################### Algorithms  ################################################
+// ###################### Algorithms  #########################################################
 
 int tarjanVisit(int &visited, int &current)
 {
     int weight = 0;
 
-    Node curr = *(nodes[current]);
-    vector<int> connections = curr.getConnections();
-    d[current] = low[current] = visited;
-    visited++; // ???
-    stackL.push(current);
+    Node curr = *(nodes[current]);                      // The vertex being visited
+    vector<int> connections = curr.getConnections();    // The adjancencies of the current vertex 
+    d[current] = low[current] = visited;                // Fill in the d and low arrays
+    visited++;
+    // Put the vertex in the stack
+    stackL.push(current);                               
     stackU[current] = true;
 
-    int children = 0;
+    int children = 0;   // Number of vertex's children
     for (int a : connections)
     {
-        //printf("Node %d checking connection %d\n", current + 1, a + 1);
         if (d[a] == -1 || stackU[a])
         {
             if (d[a] == -1)
             {
                 children++;
-                // printf("Node %d visiting %d\n", current + 1, a + 1);
                 parent[a] = current;
                 weight += tarjanVisit(visited, a);
+                // Update the size of the biggest SCC
                 if (weight > biggestSCC)
                     biggestSCC = weight;
-                //printf("Callback: Node %d visited %d\n", current + 1, a + 1);
 
-                //printf("Updating low of %d with low[current]=%d or low[%d]=%d\n", current + 1, low[current], a + 1, low[a]);
+                // Conditions if a vertex is articulation point:
                 // (1) u is root of DFS tree and has two or more chilren.
                 if (parent[current] == -1 && children > 1 && !ap[current] && !ap[current])
                 {
                     ap[current] = true;
                     ++apCount;
-                    // printf("(1)Found AP: %d\n", current + 1);
                 }
 
                 // (2) If u is not root and low value of one of its child is more
@@ -116,7 +111,6 @@ int tarjanVisit(int &visited, int &current)
                 {
                     ap[current] = true;
                     ++apCount;
-                    //printf("(2)Found AP: %d\n", current + 1);
                 }
                 low[current] = MIN(low[current], low[a]);
             }
@@ -127,12 +121,14 @@ int tarjanVisit(int &visited, int &current)
         }
     }
 
+    // Save a new SCC
     if (d[current] == low[current])
     {
         ++sccCount;
         int nodeCount = 0;
         int v = stackL.top();
         int max = v;
+        // Find the biggest id in the SCC (it is the SCC's ID)
         while (current != v)
         {
             if (v > max)
@@ -144,7 +140,7 @@ int tarjanVisit(int &visited, int &current)
 
         SCCs.push_back(new int[3]{max, nodeCount});
     }
-    //printf("Returning from %d with weight: %d\n", current + 1, weight);
+    
     if (ap[current])
         return 0;
     return weight + 1;
@@ -170,16 +166,20 @@ void SccTarjan(int nodesNum, vector<Node *> routers)
         if (d[i] == -1)
         {
             weight = tarjanVisit(visited, i);
+            // Update the size of the biggest SCC
             if (weight > biggestSCC)
                 biggestSCC = weight;
         }
 }
 
+// For the sorting algorithm
 bool compare(const int *a, const int *b)
 {
     return a[0] < b[0];
 }
 
+
+// ###################### Main ################################################################
 int main()
 {
     // Create array of router pointers
@@ -191,6 +191,7 @@ int main()
     apCount = 0;
 
     readInput();
+    // Initialize all the arrays
     d = new int[nodesNum];
     low = new int[nodesNum];
     parent = new int[nodesNum];
@@ -199,17 +200,17 @@ int main()
 
     SccTarjan(nodesNum, nodes);
 
-    // Output
-    // Number of SCCs
+    // Output:
+    // (1) Number of SCCs
     printf("%d\n", sccCount);
-    // Number of IDs
+    // (2) Number of IDs
     sort(SCCs.begin(), SCCs.end(), compare);
     printf("%d", SCCs.front()[0] + 1);
     for (vector<int *>::iterator it = SCCs.begin() + 1; it != SCCs.end(); ++it)
         printf(" %d", (*it)[0] + 1);
-    // Number of APs
+    // (3) Number of APs
     printf("\n%d\n", apCount);
-    // Size of bigges SCC without APs
+    // (4) Size of bigges SCC without APs
     printf("%d\n", biggestSCC);
 
     // Free allocs
